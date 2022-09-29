5fb1f1db494bfd3a5dc9e937f5e4252cfbcf6557
gregcotech007 <commins.greg@outlook.com>
2022-09-29 19:17:05 +0000

    Updated database url for Heroku

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ["DATABASE_URL"] = "postgres://ruoiuoahuzalya:855d2ee4ef392e5d3a9aa82e300da905be37735f05c95d09bcf543f0a8387bce@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/dcscrg73n789gg"
+os.environ["DATABASE_URL"] = "postgres://pcpmyfuuwxydjy:6a2ab0f6eb609118074636e906e8f72edd9d071aa73e98c3f151a86e11180d2a@ec2-34-243-101-244.eu-west-1.compute.amazonaws.com:5432/dc2soaudnnmp8i"
 os.environ["SECRET_KEY"] = "Cr3@tiv3B3@rdM@st3rs!!"
 os.environ["CLOUDINARY_URL"] = "cloudinary://442218363159532:hkbMmnH0u5AMhL4S6XbOUIW7YYU@gregcotech007"
