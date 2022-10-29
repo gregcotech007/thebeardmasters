753f06844f8bdfcdd7ecc0ab08c1d245e8a3c154
gregcotech007 <commins.greg@outlook.com>
2022-10-29 18:34:06 +0000

    Added bookings view and templates

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Booking
 
 # Create your views here.
+
+def all_bookings(request):
+    """ A view to show all bookings, including sorting and search queries """
+
+    bookings = Booking.objects.all()
+
+    context = {
+        'bookings': bookings,
+    }
+
+    return render(request, 'home/index.html')
