def3aa4ec8d46c1af7783f0083d12d3fbbd875bd
gregcotech007 <commins.greg@outlook.com>
2022-10-30 16:11:33 +0000

    Added search functionality

--- bookings/views.py
+++ bookings/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Booking
 
 # Create your views here.
@@ -7,9 +9,21 @@ def all_bookings(request):
     """ A view to show all bookings, including sorting and search queries """
 
     bookings = Booking.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('bookings'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            bookings = bookings.filter(queries)
 
     context = {
         'bookings': bookings,
+        'search_term': query,
     }
 
     return render(request, 'bookings/bookings.html', context)
