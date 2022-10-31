e4b640fa87901c74af2509a98c1d26d7b6ccf021
gregcotech007 <commins.greg@outlook.com>
2022-10-31 22:38:20 +0000

    Fixed issues with JS on shopping bag, Toasts not showing and update/remove item from bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from bookings.models import Booking
@@ -13,13 +13,14 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified items to the shopping bag """
 
-    booking = Booking.objects.get(pk=item_id)
+    booking = get_object_or_404(Booking, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Added {booking.name} to your bag')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {booking.name} to your bag')
@@ -31,32 +32,34 @@ def add_to_bag(request, item_id):
 def update_bag(request, item_id):
     """ Update the quantity of specific item to the shopping bag """
 
+    booking = get_object_or_404(Booking, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] += quantity
+        messages.success(request, f'Added {booking.name} to your bag')
     else:
-        bag.pop(item_bag)
+        bag.pop(item_id)
+        messages.success(request, f'Removed {booking.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
 
-# def remove_from_bag(request, item_id):
-#     """ Remove the item from the shopping bag """
+def remove_from_bag(request, item_id):
+    """ Remove the item from the shopping bag """
 
-#     try:
-#         booking = get_object_or_404(Booking, pk=item_id)
-#         bag = request.session.get('bag', {})
+    try:
+        booking = get_object_or_404(Booking, pk=item_id)
+        bag = request.session.get('bag', {})
 
-#         else:
-#             bag.pop(item_bag)
-#             messages.success(request, f'Removed {booking.name} from your bag')
+        bag.pop(item_id)
+        messages.success(request, f'Removed {booking.name} from your bag')
 
-#         request.session['bag'] = bag
-#         return HttpResponse(status=200)
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
     
-#     except Exception as e:
-#         messages.error(request, f'Error removing item: {e}')
-#         return HttpResponse(status=500)
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
