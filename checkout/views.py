0f90953715994568a0440f55fe66e2ab3a320039
gregcotech007 <commins.greg@outlook.com>
2022-11-01 12:24:54 +0000

    Added Stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Lz9hiA70jm9huLLDpZbcPhOB1DE4hgJTR8jfgoVvSwASwlaZbKJOf8hXD9aMH1ht9sw0B5FXMblhkvBArVKqlYr00ll9urqEn',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
