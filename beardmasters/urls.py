3f7afdb527d2e3a022845e7317059b1833f688f3
gregcotech007 <commins.greg@outlook.com>
2022-11-01 01:39:42 +0000

    Added checkout views and templates

--- beardmasters/urls.py
+++ beardmasters/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('bookings/', include('bookings.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
