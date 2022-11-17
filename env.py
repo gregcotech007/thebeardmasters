f5b73431a08781f415912adca4a540d27375804f
gregcotech007 <commins.greg@outlook.com>
2022-11-17 17:18:46 +0000

    Updated database url to ElephantSQL

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ["DATABASE_URL"] = "postgres://pcpmyfuuwxydjy:6a2ab0f6eb609118074636e906e8f72edd9d071aa73e98c3f151a86e11180d2a@ec2-34-243-101-244.eu-west-1.compute.amazonaws.com:5432/dc2soaudnnmp8i"
+os.environ["DATABASE_URL"] = "postgres://bpixnzpu:0lRleLPsrRtnYL1sh4R_glJ879FL83zT@lucky.db.elephantsql.com/bpixnzpu"
 os.environ["SECRET_KEY"] = "Cr3@tiv3B3@rdM@st3rs!!"
 os.environ["CLOUDINARY_URL"] = "cloudinary://442218363159532:hkbMmnH0u5AMhL4S6XbOUIW7YYU@gregcotech007"
 os.environ["STRIPE_SECRET_KEY"] = "sk_test_51Lz9hiA70jm9huLLBFRIViY1HJOevusLVKOqxr43OSoNkgs8ehxDsqa1mP0JCJDZbwO0M4pn44z0U6JrSYWU318700VzQbj8Dn"
