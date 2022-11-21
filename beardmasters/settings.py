59bdde9de13eb3d4594fa020a7ae849992b67048
gregcotech007 <commins.greg@outlook.com>
2022-11-21 18:41:19 +0000

    Updated media images and promo banner, base.css and login/register refactor

--- beardmasters/settings.py
+++ beardmasters/settings.py
@@ -186,7 +186,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 if 'USE_AWS' in os.environ:
--- env.py
+++ env.py
@@ -2,7 +2,6 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://bpixnzpu:0lRleLPsrRtnYL1sh4R_glJ879FL83zT@lucky.db.elephantsql.com/bpixnzpu"
 os.environ["SECRET_KEY"] = "Cr3@tiv3B3@rdM@st3rs!!"
-os.environ["CLOUDINARY_URL"] = "cloudinary://442218363159532:hkbMmnH0u5AMhL4S6XbOUIW7YYU@gregcotech007"
 os.environ["STRIPE_SECRET_KEY"] = "sk_test_51Lz9hiA70jm9huLLBFRIViY1HJOevusLVKOqxr43OSoNkgs8ehxDsqa1mP0JCJDZbwO0M4pn44z0U6JrSYWU318700VzQbj8Dn"
 os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51Lz9hiA70jm9huLLDpZbcPhOB1DE4hgJTR8jfgoVvSwASwlaZbKJOf8hXD9aMH1ht9sw0B5FXMblhkvBArVKqlYr00ll9urqEn"
 os.environ["STRIPE_WH_SECRET"] = "whsec_XULIgKJNMBKguXssiQURCLEsWJS3Po2s"
