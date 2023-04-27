24d881cc00621e202ab38b3311ede2ba0ac2e6bf
gregcotech007 <commins.greg@outlook.com>
2023-04-27 22:05:51 +0000

    Added link to our-story page

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('our_story/', views.our_story, name='our_story')
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def our_story(request):
+    """ A view to return the our_story page """
+
+    return render(request, 'home/our_story.html')
